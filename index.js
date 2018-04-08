c5ae4ac007f694091f7306402208ca703835b851
Sultan <sultanmr@hotmail.com>
2018-04-08 14:07:02 +0200

    deploy

--- index.js
+++ index.js
@@ -17,18 +17,71 @@ restService.post("/echo", function(req, res) {
  
   console.log ('message from webhook1');
 
-  var speech =  req.body.queryResult.parameters.echoText;
-  console.log (speech);
+  var text =  req.body.queryResult.parameters.echoText;
+
+  console.log (text);
     // req.body.result &&
     // req.body.result.parameters &&
     // req.body.result.parameters.echoText
     //   ? req.body.result.parameters.echoText
     //   : "Seems like some problem. Speak again.";
-  return res.json({
-    "speech": speech,
-    "displayText": speech,
-    "source": "webhook-echo-sample"
-  });
+  return res.json(
+    {
+      "fulfillmentText": text,
+      "fulfillmentMessages": [
+        {
+          "card": {
+            "title": "card title",
+            "subtitle": "card text",
+            "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+            "buttons": [
+              {
+                "text": "button text",
+                "postback": "https://assistant.google.com/"
+              }
+            ]
+          }
+        }
+      ],
+      "source": "example.com",
+      "payload": {
+        "google": {
+          "expectUserResponse": true,
+          "richResponse": {
+            "items": [
+              {
+                "simpleResponse": {
+                  "textToSpeech": "this is a simple response"
+                }
+              }
+            ]
+          }
+        },
+        "facebook": {
+          "text": "Hello, Facebook!"
+        },
+        "slack": {
+          "text": "This is a text response for Slack."
+        }
+      },
+      "outputContexts": [
+        {
+          "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+          "lifespanCount": 5,
+          "parameters": {
+            "param": "param value"
+          }
+        }
+      ],
+      "followupEventInput": {
+        "name": "event name",
+        "languageCode": "en-US",
+        "parameters": {
+          "param": "param value"
+        }
+      }
+    }
+  );
 });
 
 
